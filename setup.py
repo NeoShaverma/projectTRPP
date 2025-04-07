13d7dbb04cc3c20d9a931eef92c2eefa72e99da3
NeoShaverma <104695734+NeoShaverma@users.noreply.github.com>
2025-04-07 14:41:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-# Чтение зависимостей из файла requirements.txt
+
 with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
@@ -10,12 +10,12 @@ setup(
     author="Your Name",
     author_email="your.email@example.com",
     description="Telegram Monitoring Bot using Telethon for monitoring channels with marker words",
-    packages=find_packages(),  # Если структура проекта подразумевает пакеты
+    packages=find_packages(),  
     install_requires=requirements,
     python_requires='>=3.7',
     entry_points={
         'console_scripts': [
-            # Предполагается, что функция main() находится в файле bot.py
+        
             'telegram_monitor=bot:main',
         ],
     },
